1bc21fd5c9d27b870a8dce6ba9abca9b049d4ac7
hussam <chaudhry.hussam@gmail.com>
2015-05-24 15:42:10 +0200

    Dynamic read of json

--- WEB-INF/src/controller/FrontController.java
+++ WEB-INF/src/controller/FrontController.java
@@ -2,8 +2,14 @@ package controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,101 +25,113 @@ import org.esgi.web.framework.router.interfaces.IRewriter;
 
 import rewriteRules.RegExpRoute;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 
 /**
  * HttpServlet implementation class FrontController
  */
 public class FrontController extends HttpServlet implements IFrontController {
-	
+
 	private static final long serialVersionUID = 1L;
 	private IDispatcher dispatcher;
 	private IRewriter rewriter;
-	
+
 	private List<User> users;
 	private IContext context;
-	
+
 	private static String URIroot = "/FrontControllerProject/";
-	
+
 	@Override
 	public void init() throws ServletException {
 		dispatcher = new Dispatcher();
 		rewriter = new Rewriter(
-			/*new  RegExpRoute(
-				"user/([0-9]+).html",
-				"servlets.ExempleAction",
-				new String[] {"id"}
-			)*/
-//			new FileRoute()
-			new RegExpRoute(URIroot + "user/list/([a-z]+)", "modules.user.Liste", new String[] {"type"}),
-			new RegExpRoute(URIroot + "user/list", "modules.user.Liste"),
-			
-			new RegExpRoute(URIroot + "user/add", "modules.user.Create"),
-			
-			new RegExpRoute(URIroot + "user/update/([1-9]+)", "modules.user.Update", new String[] {"id"}),
-			
-			new RegExpRoute(URIroot + "home", "modules.pages.HomePage")
-		);
-		
+		/*
+		 * new RegExpRoute( "user/([0-9]+).html", "servlets.ExempleAction", new
+		 * String[] {"id"} )
+		 */
+		// new FileRoute()
+				new RegExpRoute(URIroot + "user/list/([a-z]+)",
+						"modules.user.Liste", new String[] { "type" }),
+				new RegExpRoute(URIroot + "user/list", "modules.user.Liste"),
+
+				new RegExpRoute(URIroot + "user/add", "modules.user.Create"),
+
+				new RegExpRoute(URIroot + "user/update/([1-9]+)",
+						"modules.user.Update", new String[] { "id" }),
+
+				new RegExpRoute(URIroot + "home", "modules.pages.HomePage"));
+
 		users = new ArrayList<User>();
 		ArrayList<String> rules = new ArrayList<String>();
-		
+
 		rules.add("admin");
 		rules.add("test");
-		
+
 		users.add(new User(1, "log1", "toto", rules));
 		users.add(new User(2, "log2", "titi", rules));
-		
+
 		super.init();
 	}
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		handle(request, response);
 	}
-	
+
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response) {
-		context = createContext(request,response);
-		
+		context = createContext(request, response);
+
 		context.setAttribute("users", users);
-		
+
 		rewriter.rewrite(context);
 		dispatcher.dispatch(context);
 	}
-	
-	private IContext createContext(HttpServletRequest request,HttpServletResponse response) {
-		return new Context(request,response);
+
+	private IContext createContext(HttpServletRequest request,
+			HttpServletResponse response) {
+		return new Context(request, response);
 	}
-	
-	public void getTemplate(IContext context)
-	{
+
+	public List<Entry<String, JsonNode>> getTemplate(IContext context) {
 		String requestUrl = context._getRequest().getRequestURI().toString();
 		requestUrl = requestUrl.replace("/FrontControllerProject/", "");
 		requestUrl = requestUrl.replace("/", "");
-		
+
 		ObjectMapper m = new ObjectMapper();
 		JsonNode rootNode = null;
 		try {
-			rootNode = m.readTree(new File("/home/hussam/Bureau/jee/FrontControllerProject/WEB-INF/templates/"+requestUrl+".json"));
+			rootNode = m.readTree(new File(
+					"/home/hussam/Bureau/jee/FrontControllerProject/WEB-INF/templates/"
+							+ requestUrl + ".json"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		JsonNode node = rootNode.path("module.html.layout");
 		JsonNode headerNode = node.findValue("module.html.header");
-		JsonNode header = headerNode.findValue("head");
+		List<Entry<String, JsonNode>> elements = getElementsFromNode(headerNode);
+
+		 JsonNode bodyNode = node.findValue("module.html.body");
+		 elements.addAll(getElementsFromNode(bodyNode));
+
+		 JsonNode footerNode = node.findValue("module.html.footer");
+		 elements.addAll(getElementsFromNode(footerNode));
 		
-		JsonNode bodyNode = node.findValue("module.html.body");
-		JsonNode body = bodyNode.findValue("body");
+		return elements;
+	}
+
+	public List<Entry<String, JsonNode>> getElementsFromNode(JsonNode node) {
+
+		Iterator<Entry<String, JsonNode>> it = node.fields();
 		
-		JsonNode footerNode = node.findValue("module.html.footer");
-		JsonNode footer = footerNode.findValue("footer");
-		
-		context.setAttribute("header", header.toString());
-		context.setAttribute("body", body.toString());
-		context.setAttribute("footer", footer.toString());
+		List<Entry<String, JsonNode>> myList = Lists.newArrayList(it);
+
+		return myList;
 	}
 }
--- WEB-INF/src/modules/pages/HomePage.java
+++ WEB-INF/src/modules/pages/HomePage.java
@@ -3,13 +3,17 @@ package modules.pages;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map.Entry;
 
 import org.esgi.web.framework.action.interfaces.IAction;
 import org.esgi.web.framework.context.interfaces.IContext;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import controller.FrontController;
 
-public class HomePage implements IAction{
+public class HomePage implements IAction {
 
 	@Override
 	public int setPriority(int priority) {
@@ -23,7 +27,7 @@ public class HomePage implements IAction{
 
 	@Override
 	public void addCredential(String role) {
-		
+
 	}
 
 	@Override
@@ -38,68 +42,54 @@ public class HomePage implements IAction{
 
 	@Override
 	public void proceed(IContext context) {
-//		
-//		FrontController ft = new FrontController();
-//		ft.getTemplate(context);
-		
-		String header = (String) context.getAttribute("header");
-		header = header.replace("\"", "");
-		
-		String body = (String) context.getAttribute("body");
-		body = body.replace("\"", "");
-		
-		String footer = (String) context.getAttribute("footer");
-		footer = footer.replace("\"", "");
-		
-		String content;
-		if (header == "__CURRENT__") {
-			content = (String) context.getAttribute("headerContent");
-		}
-		else {
-			content = getContentFromFile(header, "headerContent", context);
-		}
-		
-		if (body == "__CURRENT__") {
-			content += (String) context.getAttribute("bodyContent");
-		}
-		else {
-			content += getContentFromFile(body, "bodyContent", context);
-		}
-		
-		if (footer == "__CURRENT__") {
-			content += (String) context.getAttribute("footerContent");
-		}
-		else {
-			content += getContentFromFile(footer, "footerContent", context);
-		}
-		
+
+		FrontController ft = new FrontController();
+		List<Entry<String, JsonNode>> elements = ft.getTemplate(context);
+
+		String content = getContentFromFile(elements, context);
+
 		try {
 			context._getResponse().getWriter().println(content);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public String getContentFromFile(String fileName, String contentType, IContext context) {
-		
+
+	public String getContentFromFile(List<Entry<String, JsonNode>> elem,
+			IContext context) {
+
 		String content = null;
-		try (BufferedReader br = new BufferedReader(new FileReader("/home/hussam/Bureau/jee/FrontControllerProject/WEB-INF/views/"+fileName+".jsp")))
-		{
-			String sCurrentLine;
- 
-			while ((sCurrentLine = br.readLine()) != null) {
-				if (content == null)
-					content = sCurrentLine;
-				else
-					content += sCurrentLine;
+		String contentType = null;
+		String file = null;
+		String value = null;
+
+		for (int i = 0; i < elem.size(); i++) {
+
+			value = elem.get(i).toString();
+			String parts[] = value.split("=");
+			contentType = parts[0];
+			file = parts[1];
+			file = file.replace("\"", "");
+
+			try (BufferedReader br = new BufferedReader(new FileReader(
+					"/home/hussam/Bureau/jee/FrontControllerProject/WEB-INF/views/"
+							+ file + ".jsp"))) {
+				String sCurrentLine;
+
+				while ((sCurrentLine = br.readLine()) != null) {
+					if (content == null)
+						content = sCurrentLine;
+					else
+						content += sCurrentLine;
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
- 
-		} catch (IOException e) {
-			e.printStackTrace();
+			context.setAttribute(contentType, content);
+
 		}
-		
-		context.setAttribute(contentType, content);
-		
+
 		return content;
 	}
 
